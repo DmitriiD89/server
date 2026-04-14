e91dba8d37c60e93accf7d1289e9d329b15507d5
DmitriiD89 <dudikno1@gmail.com>
2026-04-14 15:21:49 +0500

    v1

--- src/index.js
+++ src/index.js
@@ -11,24 +11,27 @@ const server = http.createServer((request, response) => {
   //   const userName = url.searchParams.get("hello");
 
   if (request.url === "/users") {
-    response.status = 200;
+    response.statusCode = 200;
     response.statusMessage = "OK";
     response.header = "Content-Type: application/json";
     response.write(getUsers());
     response.end();
     return;
-  } else {
-    response.status = 400;
+  } else if (request.url === "/") {
+    response.statuscode = 404;
     response.statusMessage = "OK";
     response.header = "Content-Type: text/plain";
     response.write("Enter a name");
     response.end();
+    return;
+  } else {
+    response.status = 200;
+    response.statusMessage = "OK";
+    response.header = "Content-Type: text/plain";
+    response.write("Hello World");
+    response.end();
+    return;
   }
-  response.status = 200;
-  response.statusMessage = "OK";
-  response.header = "Content-Type: text/plain";
-  response.write("Hello World");
-  response.end();
 });
 server.listen(3000, () => {
   console.log("Сервер запущен по адресу http://127.0.0.1:3000");
