b40a5fff6c8cff858f9a13164d074fc97e8f6311
DmitriiD89 <dudikno1@gmail.com>
2026-04-14 15:15:19 +0500

    v1

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
 const http = require("http");
+const fs = require("fs");
+const path = require("path");
+const getUsers = () => {
+  const filePath = path.join(__dirname, "./data/users.json");
+  return fs.readFileSync(filePath);
+};
 const server = http.createServer((request, response) => {
-  const ip = "http://127.0.0.1";
-  const url = new URL(request.url, ip);
-  const userName = url.searchParams.get("hello");
-  if (userName) {
+  //   const ip = "http://127.0.0.1:3003";
+  //   const url = new URL(request.url, ip);
+  //   const userName = url.searchParams.get("hello");
+
+  if (request.url === "/users") {
     response.status = 200;
     response.statusMessage = "OK";
-    response.header = "Content-Type: text/plain";
-    response.write(`Hello ${userName}`);
+    response.header = "Content-Type: application/json";
+    response.write(getUsers());
     response.end();
+    return;
   } else {
     response.status = 400;
     response.statusMessage = "OK";
